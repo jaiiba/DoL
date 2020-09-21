edc73e1cbe59ed0c5d9938fc201ef807b14bee91
Jaibamon <jaibamon@outlook.com>
2020-09-21 00:52:25 -0500

    offline

--- service-worker.js
+++ service-worker.js
@@ -22,3 +22,19 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   // Perform some task
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    })
+  );
+});
