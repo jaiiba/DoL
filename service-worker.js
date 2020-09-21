4cfe54959a8cd2fa4ba076e60484f9e895896640
Jaibamon <jaibamon@outlook.com>
2020-09-21 00:43:56 -0500

    c

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,22 @@
 // Listen for install event, set callback
 self.addEventListener('install', function(event) {
-  // Perform some task
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/DoL/index.html',
+          '/DoL/main.js',
+          '/DoL/service-worker.js',
+          '/DoL/manifest.json',
+          '/DoL/icon-192.png',
+          '/DoL/icon-512.png',
+          '/DoL/favicon.ico',
+          '/DoL/DolSettingsExport.json',
+          '/DoL/style.css',
+        ]
+      );
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
